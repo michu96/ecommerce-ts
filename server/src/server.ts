08f4468024add45b1b97c8f009a1cb65ab52397a
Michal Trudzinski <mihu1701@gmail.com>
2021-05-06 15:16:51 +0200

    Init express app with prisma

---
+++ server/src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', async (_req, res) => {
+  res.send('Test')
+})
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`)
+})

47870547190aa2ac7571b611d506369da83c096e
Michal Trudzinski <mihu1701@gmail.com>
2021-05-11 18:02:06 +0200

    Init client app with redux react scss ts

---
+++ client/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './app/App'
+import { store } from './app/store'
+import { Provider } from 'react-redux'
+import './scss/index.scss'
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)

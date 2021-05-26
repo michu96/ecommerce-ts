d0a71a8d193d4926dbcb49f8e116d31835248804
Michal Trudzinski <mihu1701@gmail.com>
2021-05-26 19:03:51 +0200

    Add connected-react-router

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,10 +1,23 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
+import {
+  configureStore,
+  ThunkAction,
+  Action,
+  AnyAction,
+} from '@reduxjs/toolkit'
+import { createBrowserHistory } from 'history'
+import { connectRouter, routerMiddleware } from 'connected-react-router'
 import userReducer from '../features/user/userSlice'
+import { Reducer } from 'react'
+
+export const history = createBrowserHistory()
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    router: connectRouter(history) as Reducer<unknown, AnyAction>,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(routerMiddleware(history)),
 })
 
 export type AppDispatch = typeof store.dispatch
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ConnectedRouter } from 'connected-react-router'
 import App from './app/App'
-import { store } from './app/store'
+import { store, history } from './app/store'
 import { Provider } from 'react-redux'
 import './scss/index.scss'
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
 )
